a01b911ae39d7d2cf70e5c22f4c1eb5131f913a7
semelin <arthur@lestudio.vc>
2016-02-14 19:57:54 +0100

    created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dasboard]
   def home
   	if current_user
   		redirect_to foods_path
   	end
+  end
+
+  def dashboard
+  	@foods = current_user.foods
   end
 end
